7d5298fd3fe0097a44f38b2b177d889ec66cd304
Oleg <roxvemm2014@gmail.com>
2021-11-09 19:55:24 -0800

    initial commit

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,27 @@
 class PlantsController < ApplicationController
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: { error: "Plant not found"}, status: :not_found
+    end
+  end
+  
+#DELETE a record from the datebase 
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "Plant not found"}, status: :not_found
+    end
+  end 
+  
   # GET /plants
   def index
     plants = Plant.all
@@ -23,4 +45,5 @@ class PlantsController < ApplicationController
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
 end
